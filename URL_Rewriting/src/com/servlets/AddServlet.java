7deed9d7dfee4ef995bffcfaaaf3467d3a14db2a
Kashish4998 <kashishsahu459@gmail.com>
2020-08-19 16:48:39 +0530

    Passing Data through URL

---
+++ URL_Rewriting/src/com/servlets/AddServlet.java
@@ -0,0 +1,18 @@
+package com.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet{
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		  
+	  int n1=Integer.parseInt(req.getParameter("num1"));
+	  int n2=Integer.parseInt(req.getParameter("num2"));
+	  int k=n1+n2;
+	  res.sendRedirect("sq?k="+k);
+}
+}
